fd88d2c24059915106aa658a5d92a2f84a4698ec
naraThais <thaisna46@gmail.com>
2023-07-20 13:18:38 -0300

    Projeto1

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import http from 'node:http'
+
+const users = []
+ 
+const Server = http.createServer((req, res) => {
+    const {method, url} = req 
+
+    if(method == 'GET' && url == '/users'){
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+
+    if(method == 'POST' && url== '/users'){
+        users.push({
+            id: 1,
+            name: 'Thais',
+            email: 'Exemplo@exemplo.com',
+
+        })
+
+        return res.writeHead(201).end()
+    }
+
+    return res.end('Hello Wolrd!')
+})
+
+Server.listen(3333)
