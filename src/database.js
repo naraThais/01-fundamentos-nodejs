aa7f4b9c3d80c2d1f85639da96e7d970ccac2130
naraThais <thaisna46@gmail.com>
2023-08-01 13:48:59 -0300

    .

--- src/database.js
+++ src/database.js
@@ -1,11 +1,23 @@
 import fs from 'node:fs/promises'
 
+const databasePath = new URL('../db.json', import.meta.URL)
+console.log(databasePath)
+
 export class Database{
 
     #database = {} 
     
+    constructor(){
+        fs.readFile(databasePath, 'utf-8').then(data => {
+            this.#database = JSON.parse(data)
+        })
+        .catch(() => {
+            this.#persist()
+        })
+    }
+
     #persist(){
-        fs.writeFile('db.json', JSON.stringify(this.#database))
+        fs.writeFile(databasePath,JSON.stringify(this.#database))
     }
 
     select(table){
