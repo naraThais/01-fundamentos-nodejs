6b6fe9ec063802dd5e6833b1c55977600b5b1e7a
naraThais <thaisna46@gmail.com>
2023-08-01 09:54:38 -0300

    Buffer

---
+++ streams/buffers.js
@@ -0,0 +1,9 @@
+/*
+Buffer é uma representação de um espaço na memória do computador
+usado especificamente para transitar dados de forma rápida no computador.
+*/
+
+const buf  = Buffer.from("OK")
+
+console.log(buf)
+
--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -9,25 +9,13 @@ class InverseNumberStream extends Transform{
     }
 }
 
-// req => ReadableStream
-// res => WritableStream
-
 const server = http.createServer(async (req, res)=>{
 
-    const buffers = []
-
-    for await(const chunk of req) {
-        buffers.push(chunk)
-    }
-
-    const fullStreamContent = Buffer.concat(buffers).toString()
 
     console.log(fullStreamContent)
     return res.end(fullStreamContent)
     
-   // return req
-   // .pipe(new InverseNumberStream())
-   // .pipe(res)
+
 })
 
 server.listen(3334)
